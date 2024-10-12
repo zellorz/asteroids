4c82139e0c5df8898d74399a3bfdb9e4644aaeb9
zellorz <zellorz@gmail.com>
2024-10-12 22:58:06 +0200

    transfer for checks test

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     # Initialize pygame
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -20,6 +23,8 @@ def main():
 
         # Update the display
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
